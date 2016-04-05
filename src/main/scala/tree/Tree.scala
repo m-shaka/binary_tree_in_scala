3f06d11f15a77abfb0946ea7ebc2820cc8f4e0d0
shaka <buddha0818@gmail.com>
2016-04-05 14:33:52 +0900

    add methods(delete, list, max, min, depth)

--- src/main/scala/tree/Tree.scala
+++ src/main/scala/tree/Tree.scala
@@ -29,24 +29,49 @@ object Tree {
       case Empty => Empty
       case Node(Empty, v, r) =>
         if (v == n) r
-        else if (v < n) delete(r, n)
+        else if (v < n) Node(Empty, v, delete(r, n))
         else Empty
       case Node(l, v, Empty) =>
         if (v == n) l
         else if (v < n) Empty
-        else delete(l, n)
-      case Node(l, v, r) =>
+        else Node(delete(l, n), v, Empty)
+      case Node(l, v, r) if depth(l) > depth(r) =>
         // leftの最大値を持ってくるぞ
         if (v == n) Node(delete(l, max(l)), max(l), r)
         else if (v < n) Node(l, v, delete(r, n))
         else Node(delete(l, n), v, r)
+      case Node(l, v, r) if depth(l) <= depth(r) =>
+        // rightの最小値を持ってくるぞ
+        if (v == n) Node(l, min(r), delete(r, min(r)))
+        else if (v < n) Node(l, v, delete(r, n))
+        else Node(delete(l, n), v, r)
+
     }
 
-  def max[B >: Int](tree: Tree[Int]): B =
+  // 木の中身を昇順リストにして返す
+  def list(tree: Tree[Int]): List[Int] =
+    tree match {
+      case Empty => Nil
+      case Node(l, v, r) => list(l) ::: v :: list(r)
+    }
+
+  def max(tree: Tree[Int]): Int =
     tree match {
       case Empty => error("the tree has no content.")
       case Node(_, v, Empty) => v
       case Node(_, _, r) => max(r)
     }
 
+  def min(tree: Tree[Int]): Int =
+    tree match {
+      case Empty => error("the tree has no content.")
+      case Node(Empty, v, _) => v
+      case Node(l, _, _) => min(l)
+    }
+
+  def depth[A](tree: Tree[A]): Int =
+    tree match {
+      case Empty => 0
+      case Node(l, _, r) => 1 + (depth(l) max depth(r))
+    }
 }
